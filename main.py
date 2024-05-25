1a68b90e87ada6bc4fba2bae20756984be7c303f
janiyaNIMA <114753817+janiyaNIMA@users.noreply.github.com>
2024-05-25 06:01:36 +0530

    chtgpt code

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
-import os
 import telebot
 import requests
-from telegram import *
 
 API_TOKEN = '6463336436:AAGyGh-ANYPjMDqRLkIsCS5LjOurXvJAqDs'
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
-def strat(massage):
-    bot.reply_to(massage, "Hey..! Hows it going?")
+def start(message):
+    bot.reply_to(message, "Hey..! How's it going?")
 
 @bot.message_handler(commands=['weather'])
-def weather(massage):
-    city = "kegalle"
+def weather(message):
+    city = "Kegalle"
     API_key = 'b8a0131cac0cc7e9d7b6ba1c386f8f26'
-    units = 'standard'
+    units = 'metric'
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}&units={units}'
     resp = requests.get(url)
     msg = resp.json()
     reply = f'''
     <b>{msg['name']}</b>
-    temp = {int(msg['main']['temp'])-273}°C
-    pressure = {msg['main']['pressure']} Pa
-    humidity = {msg['main']['humidity']} %
-    wind speed = {msg['wind']['speed']} km/h
+    Temp: {msg['main']['temp']}°C
+    Pressure: {msg['main']['pressure']} hPa
+    Humidity: {msg['main']['humidity']}%
+    Wind Speed: {msg['wind']['speed']} m/s
     '''
-    bot.reply_to(massage, reply, parse_mode='html')
+    bot.reply_to(message, reply, parse_mode='html')
 
 bot.polling()
